94c06d7c1c42cf773d11e396d4d056e9e41ed600
liangyahui <liangyahui@jingdata.com>
2018-02-24 11:32:02 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # react-less-watcher
 A tool for help create-react-app work with less
+
+# Description
+It is only a simple tool for compile less to css.
+
---
+++ index.js
@@ -0,0 +1,66 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const chokidar = require('chokidar');
+const less = require('less');
+const fs = require('fs');
+
+let uri = path.resolve(process.cwd(), 'src');
+
+const argvs = process.argv;
+
+if (argvs.length === 2) {
+  console.warn('Warning: Run without -d parameter, So work with ' + uri);
+} else if (argvs.length === 4 && argvs[2] === '-d') {
+  uri = path.resolve(process.cwd(), argvs[3]);
+} else {
+  console.error('Error: Run with wrong parameter');
+  process.exit();
+}
+
+let watcher = chokidar.watch(uri, {});
+
+watcher.on('error', function(error) {
+  console.error(error);
+});
+
+watcher.on('change', function(file) {
+  if (file && !fs.statSync(file).isDirectory()) {
+    let extname = path.extname(file);
+    if (extname !== '.less') {
+      return;
+    } else {
+      let destFile = file.replace(extname, '.css');
+      fs.readFile(file, 'utf8', (e, lessInput) => {
+        if (e) {
+          console.error('err', e);
+        }
+        less
+          .render(lessInput, {
+            sourceMap: {},
+            paths: ['.', path.dirname(file)]
+          })
+          .then(output => {
+            fs.writeFileSync(destFile, output.css);
+          })
+          .catch(err => {
+            if (err) {
+              console.log('err', err);
+            }
+          })
+      });
+    }
+  }
+});
+
+watcher.on('unlink', function(file) {
+  if (file) {
+    let extname = path.extname(file);
+    if (extname !== '.less') {
+      return;
+    } else {
+      let destFile = file.replace(extname, '.css');
+      fs.unlinkSync(destFile);
+    }
+  }
+});
